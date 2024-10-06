270a5c70243a2b45773643ababe516e25377f2f2
Saeed <Saeed@hassn.com>
2024-10-06 22:02:25 +0300

    modified

---
+++ scripts/navigate.js
@@ -0,0 +1,30 @@
+function goToArtInfo() {
+    window.location.assign('/views/clubs_info/art_info.html')
+}
+function goToAstroInfo() {
+    window.location.assign('/views/clubs_info/astro_info.html')
+}
+function goToCulturalInfo() {
+    window.location.assign('/views/clubs_info/cultural_info.html')
+}
+function goToEatharInfo() {
+    window.location.assign('/views/clubs_info/eathar_info.html')
+}
+function goToJapaneseInfo() {
+    window.location.assign('/views/clubs_info/japanese_info.html')
+}
+function goToMedicalInfo() {
+    window.location.assign('/views/clubs_info/medical_info.html')
+}
+function goToMusicInfo() {
+    window.location.assign('/views/clubs_info/music_info.html')
+}
+function goToPrInfo() {
+    window.location.assign('/views/clubs_info/pr_info.html')
+}
+function goToReachInfo() {
+    window.location.assign('/views/clubs_info/reach_info.html')
+}
+function goToEquestrianInfo() {
+    window.location.assign('/views/clubs_info/equestrian_info.html')
+}
